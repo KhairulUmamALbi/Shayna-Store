dcc4593c48c6591ed6cdd11da70b002b0086942e
Khairul Umam Albi <albikhairul212@gmail.com>
2024-08-22 22:08:21 +0700

    done:convert html to vuejs

--- src/components/WomenShayna.vue
+++ src/components/WomenShayna.vue
@@ -12,20 +12,23 @@
                                     <li class="w-icon active">
                                         <a href="#"><i class="icon_bag_alt"></i></a>
                                     </li>
-                                    <li class="quick-view"><a href="product.html">+ Quick View</a></li>
+                                    <li class="quick-view"><router-link to="/product">+ Quick View</router-link></li>
                                 </ul>
                             </div>
                             <div class="pi-text">
                                 <div class="catagory-name">Coat</div>
+                                <router-link to="/product">
                                 <a href="#">
                                     <h5>Mickey Baggy</h5>
                                 </a>
+                            </router-link>
                                 <div class="product-price">
                                     $14.00
                                     <span>$35.00</span>
                                 </div>
                             </div>
                         </div>
+
                         <div class="product-item">
                             <div class="pi-pic">
                                 <img src="img/products/women-2.jpg" alt="" />
@@ -106,6 +109,7 @@
             </div>
         </div>
     </section>
+    
     <!-- Women Banner Section End -->
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProductView from '../views/ProductView.vue'
+import ShoppingCartView from '../views/ShoppingcartView.vue'
+import SuccessCart from '../views/SuccessCart.vue'
+
 
 Vue.use(VueRouter)
 
@@ -10,6 +14,7 @@ const routes = [
     name: 'home',
     component: HomeView
   },
+
   {
     path: '/about',
     name: 'about',
@@ -17,8 +22,27 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  
+  {
+    path: '/product',
+    name: 'product',
+    component: ProductView
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: ShoppingCartView
+  },
+
+  {
+    path: '/success',
+    name: 'succes',
+    component: SuccessCart
+  },
 ]
+
+
 
 const router = new VueRouter({
   routes
